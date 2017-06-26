ca3c3437964959671448b053cba179f64f3873c3
Poussiere <von.linnasta@gmail.com>
2017-06-26 16:20:25 +0200

    Update SplashActivity.java

--- app/src/main/java/com/poussiere/babybooh/SplashActivity.java
+++ app/src/main/java/com/poussiere/babybooh/SplashActivity.java
@@ -4,7 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+//Splashscreen avec un temps défini. Il semble que ce soit considéré comme une mauvaise pratique bien que assez répandu
+rivate static int SPLASH_TIME_OUT = 2000;     
 
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                
+                Intent i = new Intent (this, MainActivity.class);
+                startActivity(i);
+ 
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
+
+/*
+//SplashCreen sans durée définie (juste affiché pendant le temps de chargement qui peut être très court. C'est une bonne pratique
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -16,3 +40,4 @@ public class SplashActivity extends AppCompatActivity {
         finish();
     }
 }
+*/
