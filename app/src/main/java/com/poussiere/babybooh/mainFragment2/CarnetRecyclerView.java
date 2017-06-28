79b702b51f76c6e4fc48bb2274eb80f14e6e6cbe
poussiere <von.linnasta@gmail.com>
2017-06-28 23:47:18 +0200

    Carnet de bord transformé en recycler view

---
+++ app/src/main/java/com/poussiere/babybooh/mainFragment2/CarnetRecyclerView.java
@@ -0,0 +1,174 @@
+package com.poussiere.babybooh.mainFragment2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.poussiere.babybooh.R;
+
+import java.util.Calendar;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by poussiere on 28/06/17.
+ */
+
+
+public class CarnetRecyclerView extends RecyclerView.Adapter< CarnetRecyclerView.CarnetViewHolder> {
+
+    private int tabIcones [] = {R.drawable.ic_monstre1, R.drawable.ic_monstre2,
+            R.drawable.ic_monstre3, R.drawable.ic_monstre4, R.drawable.ic_monstre5,
+            R.drawable.ic_monstre6, R.drawable.ic_monstre7, R.drawable.ic_monstre8,
+            R.drawable.ic_monstre9, R.drawable.ic_monstre10, R.drawable.ic_monstre11,
+            R.drawable.ic_monstre12 };
+
+
+    private int quelMonstre;
+    private long date;
+    private Calendar cal;
+    private int annee;
+    private int mois;
+    private int jour;
+    private int heure;
+    private int minutes;
+    private String dateText;
+    private  String heureText;
+    private int lum;
+    private String l;
+    private String db;
+    private String d;
+    private final CarnerOnClickHandler mClickHandler;
+    private Cursor cursor;
+    private Context context;
+
+
+
+    public interface CarnerOnClickHandler {
+
+        void onEventClick(int index);
+    }
+
+
+    //Constructor
+    public CarnetRecyclerView (Context c, CarnerOnClickHandler coc)
+    {
+        mClickHandler=coc;
+        context=c;
+    }
+
+
+    @Override
+    public CarnetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, null, false);
+        CarnetViewHolder viewHolder = new CarnetViewHolder(layoutView);
+        return viewHolder;
+
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(CarnetViewHolder holder, int position) {
+
+    cursor.moveToPosition(position);
+        quelMonstre=cursor.getInt(4);
+
+        holder.ic.setImageResource(tabIcones[quelMonstre-1]);
+
+
+        // Récupération de la date et de l'heure
+        date=cursor.getLong(2);
+        cal=Calendar.getInstance();
+        cal.setTimeInMillis(date);
+        annee = cal.get(Calendar.YEAR);
+        mois = cal.get(Calendar.MONTH)+1;
+        jour = cal.get (Calendar.DAY_OF_MONTH);
+        heure = cal.get (Calendar.HOUR_OF_DAY);
+        minutes = cal.get (Calendar.MINUTE);
+
+        dateText=""+jour+"/"+mois+"/"+annee;
+
+        if (minutes==0) heureText=""+heure+":00";
+        else heureText=""+heure+":"+minutes;
+        holder.dateTv.setText(dateText);
+        holder.heureTv.setText(heureText);
+
+        //Récupération des lux
+        lum=cursor.getInt(3);
+        l = context.getString(R.string.lux);
+        holder.luxTv.setText(String.valueOf(lum)+" "+ l);
+
+
+        // Récupération des décibels et transformation en String
+        db=String.valueOf(Math.round(cursor.getDouble(1)));
+        d = context.getString(R.string.decibels);
+        holder.decTv.setText(db+" "+d);
+
+
+
+    }
+
+
+    // Return the size of the posters links array
+    @Override
+    public int getItemCount() {
+        if (cursor==null) return 0;
+        return cursor.getCount();
+    }
+
+
+    //Method to pass the array with the links of movies posters images paths to the adapter when user switch between sort by popularity and sort by top rated
+    public void setEventsCursor ( Cursor c)
+    {
+        cursor = c;
+        notifyDataSetChanged();
+    }
+
+
+
+
+
+    public class CarnetViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+
+
+        private ImageView ic;
+        private TextView dateTv;
+        private TextView heureTv;
+        private TextView luxTv;
+        private TextView decTv;
+
+        public CarnetViewHolder(View itemView) {
+            super(itemView);
+            ic=(ImageView)itemView.findViewById(R.id.imageMonstre);
+            dateTv = (TextView) itemView.findViewById(R.id.dateView);
+            heureTv = (TextView) itemView.findViewById(R.id.heureView);
+            luxTv = (TextView) itemView.findViewById(R.id.luxView);
+            decTv = (TextView) itemView.findViewById(R.id.decibelsView);
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            int adapterPosition=getAdapterPosition();
+            mClickHandler.onEventClick(adapterPosition);
+
+        }
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/poussiere/babybooh/mainFragment2/mainFragment2.java
+++ app/src/main/java/com/poussiere/babybooh/mainFragment2/mainFragment2.java
@@ -1,23 +1,141 @@
 package com.poussiere.babybooh.mainFragment2;
 
 
-import android.app.LoaderManager;
-import android.content.Loader;
+
 import android.content.Context;
+import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.content.CursorLoader;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
 import com.poussiere.babybooh.R;
+import com.poussiere.babybooh.annexes.EnregistrerActivity;
+import com.poussiere.babybooh.bdd.Contract;
 
-public class mainFragment2 extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class mainFragment2 extends Fragment implements android.app.LoaderManager.LoaderCallbacks<Cursor>, CarnetRecyclerView.CarnerOnClickHandler {
+
+    public static final int CURSOR_LOADER_ID = 42;
+    private RecyclerView rc;
+    private Cursor cursor;
+    private CarnetRecyclerView adapter;
+
+    public mainFragment2() {
+        // Required empty public constructor
+    }
 
 
+
+    public static mainFragment2 newInstance() {
+        mainFragment2 fragment = new mainFragment2();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View layoutView= inflater.inflate(R.layout.fragment_main2, container, false);
+        rc=(RecyclerView)layoutView.findViewById(R.id.lvEvenements);
+        GridLayoutManager lLayout = new GridLayoutManager(getActivity(), 1);
+
+        rc.setHasFixedSize(true);
+        rc.setLayoutManager(lLayout);
+        adapter = new CarnetRecyclerView(getActivity(), this);
+        rc.setAdapter(adapter);
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+
+
+
+    return layoutView;}
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+    @Override
+    public void onPause()
+    {super.onPause();}
+
+    @Override
+    public void onResume()
+    {super.onResume();}
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+
+            case CURSOR_LOADER_ID:
+
+                Uri getAllEvents = Contract.Evenements.URI;
+
+                return new android.content.CursorLoader(getActivity(), getAllEvents, null, null, null, null);
+
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data != null){
+            cursor=data;
+            adapter.setEventsCursor(cursor);
+    }}
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+            adapter.setEventsCursor(null);
+        }
+
+
+
+   /* @Override
+    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+        if (data != null){
+        cursor=data;
+        adapter.setEventsCursor(cursor);}
+
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+        adapter.setEventsCursor(null);
+
+    }
+*/
+
+    @Override
+    public void onEventClick(int index) {
+
+    }
+}
+/*
     protected ListView lv;
     protected MyCursorAdapter ad;
     private TextView emptyView;
@@ -117,4 +235,4 @@ public class mainFragment2 extends Fragment implements LoaderManager.LoaderCallb
     }
 
 
-}
+*/
