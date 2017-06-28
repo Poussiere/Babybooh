99ce53dbac5024a1f77f93f67ef9dfbf8ffd2438
poussiere <von.linnasta@gmail.com>
2017-06-28 23:48:51 +0200

    DetailActivity crée

---
+++ app/src/main/java/com/poussiere/babybooh/mainFragment2/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.poussiere.babybooh.mainFragment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.poussiere.babybooh.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+}
