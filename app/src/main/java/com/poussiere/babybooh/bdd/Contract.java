7638f104490888827aecfeff1fa4a82b29a5f8da
Poussiere <von.linnasta@gmail.com>
2017-07-04 17:15:29 +0200

    Update Contract.java

--- app/src/main/java/com/poussiere/babybooh/bdd/Contract.java
+++ app/src/main/java/com/poussiere/babybooh/bdd/Contract.java
@@ -20,15 +20,22 @@ public final class Contract {
     public static final class Evenements implements BaseColumns {
 
         public static final Uri URI = BASE_URI.buildUpon().appendPath(PATH_EVENEMENTS).build();
-        public static final String COLUMN_COL2 = "decibels";
+        public static final String COLUMN_COL2 = "decibels"; //decibels du cri déclencheur initial
         public static final String COLUMN_COL3 = "date";
         public static final String COLUMN_COL4 = "lumiere";
         public static final String COLUMN_COL5 = "monstre";
+        public static final String COLUMN_COL6 = "highestDecibels"; //valeur du cri le plus fort de l'évenement
+        public static final String COLUMN_COL7 = "evenementInterrompu"; // 1 si l'évenement a été interrompu par l'utilisateur, 0 sinon
+        publci static final String COMUMN_COL8 = "duree"; //indique la durée totale du réveil avant rendormissement ou arrêt utilisateur
+        
         public static final int POSITION_ID = 0;
         public static final int POSITION_COL2 = 1;
         public static final int POSITION_COL3= 2;
         public static final int POSITION_COL4 = 3;
         public static final int POSITION_COL5 = 4;
+        public static final int POSITION_COL6 = 5;
+        public static final int POSITION_COL7 = 6;
+        public static final int POSITION_COL8 = 7;
        
        /* public static final ImmutableList<String> EVENEMENT_COLUMNS = ImmutableList.of(
                 _ID,
