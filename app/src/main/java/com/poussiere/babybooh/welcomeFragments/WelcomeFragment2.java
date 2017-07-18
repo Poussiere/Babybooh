fb6e001a3db73c551cba91ce7d61c46f6e4c6636
Poussiere <von.linnasta@gmail.com>
2017-07-18 17:05:43 +0200

    Update WelcomeFragment2.java

--- app/src/main/java/com/poussiere/babybooh/welcomeFragments/WelcomeFragment2.java
+++ app/src/main/java/com/poussiere/babybooh/welcomeFragments/WelcomeFragment2.java
@@ -15,50 +15,45 @@ import com.poussiere.babybooh.R;
  * create an instance of this fragment.
  */
 public class WelcomeFragment2 extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
+   
+    
+    private ImageView welcomeLogo2;
+    
     public WelcomeFragment2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WelcomeFragment2.
-     */
-    // TODO: Rename and change types and number of parameters
+  
     public static WelcomeFragment2 newInstance() {
         WelcomeFragment2 fragment = new WelcomeFragment2();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
+  
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+       
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_fragment2, container, false);
+        View layoutView = inflater.inflate(R.layout.fragment_welcome_fragment2, container, false);
+        
+        welcomeLogo2=(ImageView)layoutView.findViewById(R.id.welcome_logo2);
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        double heightD = width*0.65; // 0,56 est le ratio de l'image
+        int height=(int)heightD;
+        FrameLayout.LayoutParams parms = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,height);
+        welcomeLogo2.setLayoutParams(parms);
+
+        return layoutView;
+        
     }
 
 }
