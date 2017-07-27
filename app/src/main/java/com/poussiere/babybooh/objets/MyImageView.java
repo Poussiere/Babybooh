4055509a0279a60cbec41fd6f08f847977029bcd
Poussiere <von.linnasta@gmail.com>
2017-07-27 16:44:13 +0200

    Update MyImageView.java

--- app/src/main/java/com/poussiere/babybooh/objets/MyImageView.java
+++ app/src/main/java/com/poussiere/babybooh/objets/MyImageView.java
@@ -5,4 +5,26 @@ package com.poussiere.babybooh.objets;
  */
 
 public class MyImageView {
+  
+   public MyImageView (Context context) {
+        super(context);
+        setScaleType(ScaleType.MATRIX);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Drawable d = getDrawable();
+        if (d != null) {
+            Matrix matrix = new Matrix();
+            RectF src = new RectF(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+            RectF dst = new RectF(0, 0, w, h);
+            matrix.setRectToRect(src, dst, Matrix.ScaleToFit.CENTER);
+            float[] points = {
+                    0, d.getIntrinsicHeight()
+            };
+            matrix.mapPoints(points);
+            matrix.postTranslate(0, h - points[1]);
+            setImageMatrix(matrix);
+        }
+    }
 }
